e3734d60f11e12b2f4a888d6ba3d3daf018abf48
Bibash Shrestha <bibashshrestha@gmail.com>
2022-01-29 15:55:26 +0545

    hydrated-bloc -rememeber data update

--- lib/counter/cubit/counter_cubit.dart
+++ lib/counter/cubit/counter_cubit.dart
@@ -1,12 +1,22 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'counter_state.dart';
 
-class CounterCubit extends Cubit<CounterState> {
+class CounterCubit extends Cubit<CounterState> with HydratedMixin {
   CounterCubit() : super(const CounterInitial(count: 0));
 
   void increment() => emit(CounterIncreased(count: state.count! + 1));
 
   void decrement() => emit(CounterDecreased(count: state.count! - 1));
+
+  @override
+  CounterState? fromJson(Map<String, dynamic> json) {
+    return CounterInitial(count: json['count'] as int);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(CounterState state) {
+    return <String, dynamic>{'count': state.count};
+  }
 }
